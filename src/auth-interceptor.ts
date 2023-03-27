7059471927be4f77a881d72445fa9845793b5081
ElkinDeArmasTissini <elkinar@tissini.com>
2023-03-27 10:35:07 -0500

    [ADD] add logout

--- src/auth-interceptor.ts
+++ src/auth-interceptor.ts
@@ -8,10 +8,11 @@ import {
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { AuthService } from './app/services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   intercept(
     request: HttpRequest<any>,
@@ -30,6 +31,7 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
+          this.authService.setIsAuthenticated(false);
           this.router.navigate(['/login']);
         }
 
